8cfa0577d136f2f7886412b25da02d78bcb44258
Filippo Pinton <filippo.pinton@thron.com>
2023-01-15 15:04:55 +0100

    feat(ButterneckAwscdkPythonApp): add basic project type
    Add new project type with basic backstage catalog info generation

---
+++ test/awscdk-python-app.test.ts
@@ -0,0 +1,18 @@
+import { ButterneckAwsCdkPythonApp } from '../src';
+
+test('ButterneckAwscdkPythonAppProject', () => {
+
+  const project = new ButterneckAwsCdkPythonApp({
+    name: 'foo-project',
+    moduleName: 'foo',
+    authorName: 'Foo Bar',
+    authorEmail: 'foo.bar@example.com',
+    cdkVersion: '2.1.0',
+    version: '0.0.1',
+  });
+
+  const fileNames = project.files.map(_ => _.path);
+  ['requirements.txt', 'requirements-dev.txt', 'cdk.json'].forEach(fileName => {
+    expect(fileNames).toContain(fileName);
+  });
+});
