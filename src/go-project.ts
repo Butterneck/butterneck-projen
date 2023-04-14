db288564c7a062b4d319daf79a066dca99e4863e
Filippo Pinton <pinton.filippo@protonmail.com>
2023-04-14 22:24:13 +0200

    feat(GoProject): add scaffold

--- src/go-project.ts
+++ src/go-project.ts
@@ -1,4 +1,4 @@
-import { Project, ProjectOptions } from 'projen';
+import { Project, ProjectOptions, Component, SampleFile } from 'projen';
 
 /**
  * TODO
@@ -22,5 +22,52 @@ export interface GoProjectOptions extends ProjectOptions {
 export class GoProject extends Project {
   constructor(options: GoProjectOptions) {
     super(options);
+
+    new GoMod(this);
+    new GoMainCode(this);
+  }
+}
+
+/**
+ * TODO
+ *
+ * @class GoMod
+ * @extends {Component}
+ */
+class GoMod extends Component {
+  constructor(project: GoProject) {
+    super(project);
+
+    new SampleFile(project, 'go.mod', {
+      contents: [
+        `module github.com/butterneck/${project.name}/src`,
+        '',
+        'go 1.20',
+      ].join('\n'),
+    });
+  }
+}
+
+/**
+ * TODO
+ *
+ * @class GoMainCode
+ * @extends {Component}
+ */
+class GoMainCode extends Component {
+  constructor(project: GoProject) {
+    super(project);
+
+    new SampleFile(project, '', {
+      contents: [
+        'package main',
+        '',
+        'import "fmt"',
+        '',
+        'func main() {',
+        '    fmt.Println("Hello, World!")',
+        '}',
+      ].join('\n'),
+    });
   }
 }
